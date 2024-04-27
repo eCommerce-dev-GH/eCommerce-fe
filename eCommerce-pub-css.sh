4092be54b6deb6b5a4f459ac655a3bf97ca13181
Cloud User <ec2-user@ip-172-31-2-120.us-east-2.compute.internal>
2024-04-27 01:34:50 +0000

    added css file and changed eCommerce-pub.sh to eCommerce-pub-html.sh

---
+++ eCommerce-pub-css.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# ecommerce website for front end CSS format
+
+/* Reset styles */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    font-family: Arial, sans-serif;
+    line-height: 1.6;
+}
+
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 20px;
+    text-align: center;
+}
+
+nav ul {
+    list-style-type: none;
+}
+
+nav ul li {
+    display: inline;
+    margin-right: 20px;
+}
+
+nav ul li a {
+    color: #fff;
+    text-decoration: none;
+}
+
+main {
+    padding: 20px;
+}
+
+footer {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px 0;
+}
+
